e4bb1de8f8f6f3734d30abb605125aa616c39ee3
maryam-manz <ma6056@nyu.edu>
2024-12-12 20:42:22 -0500

    Update compare_with_exact.py
    only kept the animation with 800 frames test

--- lower_order_1d/compare_with_exact.py
+++ lower_order_1d/compare_with_exact.py
@@ -10,14 +10,14 @@ import numpy as np
 # t, x, rho, v, p = load_data("sodshock200.npz")
 # # animate(t, x, np.abs(rho - rho_e) / rho_e, np.abs(v - v_e), np.abs(p - p_e) / p_e)
 
-residuals_animation("testsodshock200.npz", '../exactsodshock1d/exactsodshock200.npz', legend1='Simulation', legend2='Exact')
+# residuals_animation("testsodshock200.npz", '../exactsodshock1d/exactsodshock200.npz', legend1='Simulation', legend2='Exact')
 
 #-----400 grid points-----#
 # t_e, x_e, rho_e, v_e, p_e = load_data('../exactsodshock1d/exactsodshock400.npz')
 # t, x, rho, v, p = load_data("sodshock400.npz")
 # animate(t, x, np.abs(rho - rho_e) / rho_e, np.abs(v - v_e), np.abs(p - p_e) / p_e)
 
-residuals_animation("testsodshock400.npz", '../exactsodshock1d/exactsodshock400.npz', legend1='Simulation', legend2='Exact')
+# residuals_animation("testsodshock400.npz", '../exactsodshock1d/exactsodshock400.npz', legend1='Simulation', legend2='Exact')
 
 #-----800 grid points-----#
 # t_e, x_e, rho_e, v_e, p_e = load_data('../exactsodshock1d/exactsodshock800.npz')
